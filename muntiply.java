aee72421f071b9755405faa5ca6d1c6703d55ae8
Aman Kumar <amansamastipur123@gmail.com>
2026-09-12 20:17:19 +0530

    adding in different branch of muntiply

---
+++ muntiply.java
@@ -0,0 +1,14 @@
+public class MultiplicationOfTwoNumbers {
+
+    public static void main(String[] args) {
+
+        int a = 4;
+        int b = 6;
+
+        int multiplication = a * b;
+
+        System.out.println("First number: " + a);
+        System.out.println("Second number: " + b);
+        System.out.println("Multiplication of two numbers: " + multiplication);
+    }
+}
