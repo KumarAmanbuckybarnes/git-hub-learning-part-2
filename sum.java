4268694a90da608251ea01b870378b206499a8d4
Aman Kumar <amansamastipur123@gmail.com>
2026-09-12 19:36:28 +0530

    intial commit

---
+++ sum.java
@@ -0,0 +1,14 @@
+public class SumOfTwoNumbers {
+
+    public static void main(String[] args) {
+
+        int a = 4;
+        int b = 6;
+
+        int sum = a + b;
+
+        System.out.println("First number: " + a);
+        System.out.println("Second number: " + b);
+        System.out.println("Sum of two numbers: " + sum);
+    }
+}
